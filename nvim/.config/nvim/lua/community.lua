5d841cab6f55daef80aba1e5a31646b75b5d6ead
jveliz@natureconnect.eco <jveliz@BACKEND-VM-03.egs.local>
2025-03-20 10:03:47 +0000

    updates on neovim config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,5 +1,5 @@
 return {
 	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
+	{ import = "astrocommunity.colorscheme.gruvbox-nvim" },
 	-- import/override with your plugins folder
 }
